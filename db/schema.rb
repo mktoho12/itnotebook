31efe05863e307e0cb7bf269d59835f6bdcbacac
mktoho12 <makoto_16n1200102@nnn.ed.jp>
2019-07-26 03:23:01 +0900

    postsテーブルのimage_nameカラムは必要ないので削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_18_034153) do
+ActiveRecord::Schema.define(version: 2019_07_25_181913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2019_07_18_034153) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_name"
   end
 
   create_table "users", force: :cascade do |t|
